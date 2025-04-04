1e1e5fc3e42694f1830b6f1b855ce1df1fac79f6
MahmoudHany <mahmoud.hany.attia@gmail>
2025-04-04 04:10:07 +0200

    update BookRating widget in book details view

--- lib/Features/home/presentation/views/widgets/book_rating.dart
+++ lib/Features/home/presentation/views/widgets/book_rating.dart
@@ -1,18 +1,19 @@
-
 import 'package:bookly/core/Utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
-    return  Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
+          size: 14,
           color: Color(0xFFFFDD4F),
         ),
         const SizedBox(width: 6.3),
@@ -21,10 +22,13 @@ class BookRating extends StatelessWidget {
           style: Styles.textStyle16,
         ),
         const SizedBox(width: 5),
-        Text(
-          '(254)',
-          style: Styles.textStyle14.copyWith(
-            color: const Color(0xFF707070),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(254)',
+            style: Styles.textStyle14.copyWith(
+              
+            ),
           ),
         )
       ],
