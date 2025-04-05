f3a4e6208f1a6a2b94b30862f8d47a2dc973b775
MahmoudHany <mahmoud.hany.attia@gmail>
2025-04-06 00:30:45 +0200

    refactor book details view by creating separate sections for book details and similar books

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,9 +1,6 @@
-import 'package:bookly/Features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar_book_details.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/custom_book_item.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/similar_books_list_view.dart';
-import 'package:bookly/core/Utils/styles.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -11,71 +8,23 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
+            padding: EdgeInsets.symmetric(horizontal: 30),
             child: Column(
               children: [
-                const CustomAppBarBookDetails(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * .19),
-                  child: const CustomBookItem(),
-                ),
-                const SizedBox(
-                  height: 33,
-                ),
-                Text(
-                  'The Jungle Book',
-                  style: Styles.textStyle30.copyWith(
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Opacity(
-                  opacity: .7,
-                  child: Text(
-                    'Rudyard Kipling',
-                    style: Styles.textStyle18.copyWith(
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                const BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                const SizedBox(
-                  height: 37,
-                ),
-                const BooksAction(),
-                const Expanded(
+                CustomAppBarBookDetails(),
+                BooksDetailsSection(),
+                Expanded(
                   child: SizedBox(
                     height: 50,
                   ),
                 ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Book Description',
-                    style: Styles.textStyle14.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                const SimilarBooksListView(),
-                const SizedBox(
+                SimilarBooksSection(),
+                SizedBox(
                   height: 40,
                 ),
               ],
@@ -86,5 +35,3 @@ class BookDetailsViewBody extends StatelessWidget {
     );
   }
 }
-
-
