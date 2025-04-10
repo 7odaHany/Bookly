e84281db943a3425daba2e691fe4aba6b4da6d31
MahmoudHany <mahmoud.hany.attia@gmail>
2025-04-10 02:18:46 +0200

    add fetchSimilarBooks method to HomeRepo and its implementation

--- lib/Features/home/data/repos/home_repo_implementation.dart
+++ lib/Features/home/data/repos/home_repo_implementation.dart
@@ -45,4 +45,23 @@ class HomeRepoImpl extends HomeRepo {
       return Left(ServerFailure(e.toString()));
     }
   }
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String categoryId}) async {
+    try {
+      var data = await apiService.get(
+          endPoind:
+              'volumes?Filtering=free-ebooks&q=subject:fun&sorting=relevance');
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
+    }
+  }
 }
