5998c83d7a43b3136e2204b937c1978880dd57d5
MahmoudHany <mahmoud.hany.attia@gmail>
2025-05-16 21:30:17 +0300

    add home entites

---
+++ lib/Features/home/domain/entities/home_entity.dart
@@ -0,0 +1,14 @@
+class HomeEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final num rating;
+  final num price;
+
+  HomeEntity(
+      {required this.image,
+      required this.title,
+      required this.authorName,
+      required this.rating,
+      required this.price});
+}
